36fa7d72fe09ea8ce2c6c2a0a29784c2320fab98
Alisson R S Prestes <javalisson@gmail.com>
2019-09-12 17:08:46 +0000

    Novo endpoint para adicionar itens a serem produzidos

--- core/app.js
+++ core/app.js
@@ -24,14 +24,14 @@ const connection = mysql.createConnection({
   database: process.env.MYSQL_DATABASE
 })
 
-// connection.connect(err => {
-//   if (err) {
-//     console.error("Falha ao conectar ao banco de dados:", err)
-//     throw err
-//   } else {
-//     console.log('Connected to database with success')
-//   }
-// })
+connection.connect(err => {
+  if (err) {
+    console.error("Falha ao conectar ao banco de dados:", err)
+    throw err
+  } else {
+    console.log('Connected to database with success')
+  }
+})
 
 app.get('/api', (req, res) => {
   res.send('Hello API!')
@@ -42,9 +42,35 @@ app.post('/api/echo', (req, res) => {
   res.send(req.body)
 })
 
+// insert com POST
+app.post('/api/pizza', (req, res) => {
+  let id = req.body.id
+  let sabor = req.body.sabor
+  let status = req.body.status
+  let sql = `INSERT INTO pedidos (id, sabor, status) VALUES (${id}, '${sabor}', '${status}')`
+  connection.query(sql, (err, result) => {
+    if (err) throw err
+    console.log(result)
+    res.send(result)
+  })
+})
+
+// insert com GET
+app.get('/api/nova-pizza/:id/:sabor/:status', (req, res) => {
+  let id = req.params.id
+  let sabor = req.params.sabor
+  let status = req.params.status
+  let sql = `INSERT INTO pedidos (id, sabor, status) VALUES (${id}, '${sabor}', '${status}')`
+  connection.query(sql, (err, result) => {
+    if (err) throw err
+    console.log(result)
+    res.send(result)
+  })
+})
+
 app.get('/api/pizza/:id', (req, res) => {
   let id = req.params.id
-  let sql = `SELECT A.*, B.* FROM pedidos A JOIN sabores B ON A.sabor = B.sabor WHERE A.id = '${id}'`
+  let sql = `SELECT A.id, A.sabor, A.status, B.descricao FROM pedidos A JOIN sabores B ON A.sabor = B.sabor WHERE A.id = '${id}'`
   connection.query(sql, (err, result) => {
     if (err) throw err
     console.log(result)
@@ -80,7 +106,7 @@ app.get('/api/altera-status-pizza/:id/:status', (req, res) => {
 
 app.get('/api/pizzas/:status', (req, res) => {
   let status = req.params.status
-  let sql = `SELECT A.*, B.* FROM pedidos A JOIN sabores B ON A.sabor = B.sabor WHERE A.status = '${status}'`
+  let sql = `SELECT A.id, A.sabor, A.status, B.descricao FROM pedidos A JOIN sabores B ON A.sabor = B.sabor WHERE A.status = '${status}' ORDER BY A.id ASC`
   connection.query(sql, (err, result) => {
     if (err) throw err
     console.log(result)
